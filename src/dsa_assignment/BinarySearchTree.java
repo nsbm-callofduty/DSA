a18d127ad7fae61100cfbba385b2acf37dc2b626
Yassa123 <yasitha.ishantha@gmail.com>
2014-02-23 11:16:03 +0530

    DSA_Project at github.com

---
+++ src/dsa_assignment/BinarySearchTree.java
@@ -0,0 +1,57 @@
+package dsa_assignment;
+
+ 
+public class BinarySearchTree {
+         
+        Node root;
+        
+        public void add(int data){
+            
+            Node nodeToAdd = new Node(data);
+            
+            if (root == null)
+                root = nodeToAdd;
+            
+            traverseAddAddNode(root,nodeToAdd);
+                                
+        }
+        
+        private void traverseAddAddNode(Node node,Node nodeToAdd){
+            
+            if (nodeToAdd.data < node.data){
+                
+                if (node.leftchild == null){
+                    
+                    node.leftchild = nodeToAdd;
+                }
+                else{
+                    
+                    traverseAddAddNode(node.leftchild,nodeToAdd);
+                }
+                if (node.rightchild == null){
+                    
+                    node.rightchild = nodeToAdd;
+                }
+                else{
+                    
+                    traverseAddAddNode(node.rightchild,nodeToAdd);
+                }
+            }
+        }
+         public static void main(String[]args){
+        
+            BinarySearchTree tree = new BinarySearchTree();
+        
+            tree.add(50);
+            tree.add(40);
+            tree.add(45);
+            tree.add(55);
+            tree.add(60);
+            tree.add(53);
+            System.out.println("hi");
+        
+        
+        
+    }
+
+}
---
+++ src/dsa_assignment/Node.java
@@ -0,0 +1,16 @@
+package dsa_assignment;
+
+
+class Node {
+    int data;
+    Node leftchild;
+    Node rightchild;
+    
+    public Node(){
+        
+    }
+    
+    public Node(int data){
+        this.data = data ;
+    }
+}
