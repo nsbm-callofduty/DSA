a66e2e39fb71a59d6f07831789c6b5f362ddb1eb
shahiq <shahiqahamed@gmail.com>
2014-03-01 00:42:44 +0530

    Merge origin/master
    Conflicts:
    	src/dsa_assignment/BinarySearchTree.java
    	src/dsa_assignment/Node.java

--- src/dsa_assignment/BinarySearchTree.java
+++ src/dsa_assignment/BinarySearchTree.java
@@ -232,31 +232,4 @@ public class BinarySearchTree {
         }
         return s;
     }
-
-    public static void main(String[] args) {
-
-        BinarySearchTree theTree = new BinarySearchTree();
-        theTree.addNode(6786001191251d, "Thinking In Java", "Bruce", "Eckel");
-        theTree.addNode(3786001191062d, "The elements of Java style", "Scott", "Ambler");
-        theTree.addNode(3786001184075d, "Effective Java", "Joshua", "Bloch");
-        theTree.addNode(9786001183990d, "Head First design patterns", "Eric", "Freeman");
-        theTree.addNode(8785945583905d, "PHP and MySQL Web Development", "Luke", "Welling");
-        theTree.addNode(8727089728320d, "PHP 4 Bible", "Tim", "Converse");
-        theTree.addNode(1727089655864d, "PHP 4: A Beginner's Guide", "Bill", "McCarty");
-        theTree.addNode(2786325955707d, "PHP Functions Essential Reference", "Torben", "Wilson");
-        theTree.addNode(1786703422290d, "Programming PHP ", "Rasmus", "Lerdorf");
-        theTree.addNode(1130588189933d, "PHP Functions Essential Reference", "Torben", "Wilson");
-        theTree.addNode(2115514789655d, "PHP Object-Oriented Solutions ", "David", "Powers");
-        theTree.addNode(7561993138507d, "Head First PHP & MySQL", "Michael", "Morrison");
-        theTree.addNode(1222180496829d, "Head First Java, 2nd Edition ", "Kathy", "Sierra");
-        theTree.addNode(4993355853224d, "Java: How to Program, 8th Edition", "P.J.", "Deitel");
-        theTree.addNode(9052083632623d, "The Java Programming Language, 4th Edition ", "Ken ", "Arnold");
-        theTree.addNode(5600296084786d, "Securing PHP Web Applications ", "Tricia", "Ballad");
-        theTree.addNode(8448938360606d, "Java Concurrency in Practice", "Brian  ", "Goetz");
-        theTree.addNode(3472479731436d, "Java Generics and Collections ", "Philip  ", "Wadler");
-        theTree.addNode(7124428189487d, "Java In A Nutshell, 5th Edition ", "David  ", "Flanagan");
-        theTree.addNode(1001001001001d, "Java", "Dinish ", "De Silva");
-
-    }
-
 }
